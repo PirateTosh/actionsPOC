58852d7e5b5ecc5a0856fd91070630054d25f5ba
Paritosh Singh <paritosh.singh@xorlabs.com>
2024-01-31 18:16:13 +0530

    updated to a flask app

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-# python program to print "Hello World" 
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
