a6a058721f3f3cb423f132759b947ebe2e382888
Paritosh Singh <paritosh.singh@xorlabs.com>
2024-01-31 20:03:02 +0530

    job update

--- run.py
+++ run.py
@@ -1,12 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# python program to print "Hello World"
+print("Hello World")
